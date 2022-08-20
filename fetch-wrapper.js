c448c489616379710a51db4f4cd7e59143b33950
Wesley <milo974@gmail.com>
2022-08-20 23:15:24 +0400

    indent

--- fetch-wrapper.js
+++ fetch-wrapper.js
@@ -1,19 +1,19 @@
 export default class FetchWrapper{
-    constructor(baseURL){
-        this.baseURL = baseURL;
-    }
+  constructor(baseURL){
+    this.baseURL = baseURL;
+  }
 
-    get(endpoint) {
-        return fetch(this.baseURL + endpoint).then(
-            response => response.json()
-        )
-    }
+  get(endpoint) {
+  	return fetch(this.baseURL + endpoint).then(
+  		response => response.json()
+  	)
+  }
 	
 	put(endpoint, body){
 		return fetch(this.baseURL + endpoint, {
-		    method: "put",
-		    headers: {"Content-Type":"application/json"},
-		    body: JSON.stringify(body)
+		  method: "put",
+		  headers: {"Content-Type":"application/json"},
+		  body: JSON.stringify(body)
 		})
 		.then(response => response.json())        
 	}
